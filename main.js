a3cfbeab2b54ce7040d502b9e6591b65af9f1bb4
chris <christopherscaudill@gmail.com>
2021-01-24 17:58:28 -0500

    attribute binding

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const appY = Vue.createApp({
-    data:  function() {
+    data: function() {
         return {
-            product: 'Super Socks'
+            product: 'Super Socks',
+            description: 'Worlds most comfortable socks!',
+            starRating: '5',
+            starCustomers: '3',
+            image: './assets/images/socks_blue.jpg',
+            urlInfo: 'https://en.wikipedia.org/wiki/Sock'
         }
     }
 })
