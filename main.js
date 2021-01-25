2797e3e74768d5bf1d9b2f3cc95bff6cad4e5d22
chris <christopherscaudill@gmail.com>
2021-01-24 22:28:38 -0500

    class & style binding

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const appY = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             urlInfo: 'https://en.wikipedia.org/wiki/Sock',
             inventory: 11,
+            inStock: true,
+            lowInventoryLevel: false,
             sale: false,
             details: ['50% cotton', '10% luck', '20% skill', '15% concentrated power of will', '5% pleasure'],
             variants: [{
@@ -22,15 +24,34 @@ const appY = Vue.createApp({
                 }
             ],
             sizes: ['S', 'M', 'L'],
-            cart: 0
+            cart: 0,
         }
     },
     methods: {
+        updateInventory() {
+            if (this.inventory == 0) {
+                this.inStock = false
+            } else {
+                this.inStock = true
+            }
+            this.lowInventoryLevel = this.lowInventoryCheck()
+        },
+        lowInventoryCheck() {
+            if (this.inventory <= 10 && this.inventory > 0) {
+                return true
+            } else {
+                return false
+            }
+        },
         addToCart() {
             this.cart += 1
+            this.inventory -= 1
+            this.updateInventory()
         },
         removeFromCart() {
             this.cart -= 1
+            this.inventory += 1
+            this.updateInventory()
         },
         updateImage(variantImage) {
             this.image = variantImage
