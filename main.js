aef387fc5f98ee3ead38c56186c2d1c19ac9cab5
chris <christopherscaudill@gmail.com>
2021-01-24 20:47:20 -0500

    list rendering

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const appY = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             urlInfo: 'https://en.wikipedia.org/wiki/Sock',
             inventory: 11,
-            sale: false
+            sale: false,
+            details: ['50% cotton', '10% luck', '20% skill', '15% concentrated power of will', '5% pleasure'],
+            variants: [{
+                    id: 0001,
+                    color: 'blue'
+                },
+                {
+                    id: 0002,
+                    color: 'green'
+                }
+            ],
+            sizes: ['S', 'M', 'L']
         }
     }
 })
